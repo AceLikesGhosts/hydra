7a9d5aa8e10f148a241deb59923f59f2e1d17c94
AceLikesGhosts <acelikesghosts@gmail.com>
2022-09-25 13:00:19 -0700

    exports

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,19 @@
 // Export everything.
 import hydra from './hydra/hydra';
+import { Queue, IQueue, ILinkedList, LinkedList, Node, Stack, IStack } from './structures';
 import type { HydraObject, HydraReturn } from './types/hydraReturns';
 
 export default hydra;
+export
+{
+    Queue,
+    IQueue,
+    LinkedList,
+    ILinkedList,
+    Node,
+    Stack,
+    IStack,
+};
 export type {
     HydraObject,
     HydraReturn
